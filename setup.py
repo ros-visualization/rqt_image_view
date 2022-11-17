ad24acbecba0ab774bdfe80d410c6c64623778fc
Audrow Nash <audrow@openrobotics.org>
2022-11-17 10:29:54 -0600

    [master] Update maintainers - 2022-11-07 (#73)

--- setup.py
+++ setup.py
@@ -6,7 +6,9 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     packages=['rqt_image_view'],
     package_dir={'': 'src'},
-    scripts=['scripts/rqt_image_view']
+    scripts=['scripts/rqt_image_view'],
+    maintainer='William Woodall',
+    maintainer_email='william@openrobotics.org',
 )
 
 setup(**d)
